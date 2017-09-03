d0ad8b7e904c640220ddc9a8baaa8cccc8e8deb9
Daniel <db2791@nyu.edu>
2017-09-02 20:40:22 -0500

    nohup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+#####################################################################
+########### MAKE SURE TO CHANGE NODE TO RUN IN BACKGROUND ###########
+#####################################################################
+
 provider "aws" {
 	"region" = "${var.region}"
 	"access_key" = "${var.access_key}"
@@ -46,26 +50,26 @@ resource "aws_instance" "client_server" {
 	}
 }
 
-resource "aws_instance" "proxy_server" {
-	ami = "ami-4b133c5d"
-	instance_type = "t2.nano"
-	key_name = "fre"
-	vpc_security_group_ids = ["${aws_security_group.api_server.id}"]
-
-	tags {
-		Name = "proxy_server"
-	}
-
-	provisioner "remote-exec" {
-		script = "./proxy_provisioner.sh"
-
-		connection {
-			user = "ubuntu"
-			private_key = "${file("~/Downloads/fre.pem")}"
-			type = "ssh"
-		}
-	}
-}
+#resource "aws_instance" "proxy_server" {
+#	ami = "ami-4b133c5d"
+#	instance_type = "t2.nano"
+#	key_name = "fre"
+#	vpc_security_group_ids = ["${aws_security_group.api_server.id}"]
+#
+#	tags {
+#		Name = "proxy_server"
+#	}
+#
+#	provisioner "remote-exec" {
+#		script = "./proxy_provisioner.sh"
+#
+#		connection {
+#			user = "ubuntu"
+#			private_key = "${file("~/Downloads/fre.pem")}"
+#			type = "ssh"
+#		}
+#	}
+#}
 
 resource "aws_security_group" "api_server" {
 	name = "api_server"
@@ -93,6 +97,6 @@ output "client_ip" {
 	value = "${aws_instance.client_server.public_ip}"
 }
 
-output "proxy_ip" {
-	value = "${aws_instance.proxy_server.public_ip}"
-}
+#output "proxy_ip" {
+#	value = "${aws_instance.proxy_server.public_ip}"
+#}
