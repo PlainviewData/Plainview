b037cacf4c7e4563f5c91345abbab73950c292e2
Daniel <db2791@nyu.edu>
2017-09-04 16:07:39 -0500

    dsa

--- webserver/nginx_config.py
+++ webserver/nginx_config.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -6,17 +7,17 @@ parser.add_argument('--client_server_ip')
 parser.add_argument('--proxy_server_ip')
 args = parser.parse_args()
 
-with open("~/plainview/webserver/default", "rt") as fin:
-    with open("~/plainview/webserver/default", "wt") as fout:
+with open(os.path.join("/home/ubuntu/plainview/webserver", "default_template.txt"), "rt") as fin:
+    with open(os.path.join("/home/ubuntu/plainview/webserver", "default"), "wt") as fout:
         for line in fin:
             fout.write(line.replace('{{api_server_ip}}', args.api_server_ip))
 
-with open("~/plainview/webserver/default", "rt") as fin:
-    with open("~/plainview/webserver/default", "wt") as fout:
+with open(os.path.join("/home/ubuntu/plainview/webserver", "default_template.txt"), "rt") as fin:
+    with open(os.path.join("/home/ubuntu/plainview/webserver", "default"), "wt") as fout:
         for line in fin:
             fout.write(line.replace('{{client_server_ip}}', args.client_server_ip))
 
-with open("~/plainview/webserver/default", "rt") as fin:
-    with open("~/plainview/webserver/default", "wt") as fout:
+with open(os.path.join("/home/ubuntu/plainview/webserver", "default_template.txt"), "rt") as fin:
+    with open(os.path.join("/home/ubuntu/plainview/webserver", "default"), "wt") as fout:
         for line in fin:
             fout.write(line.replace('{{proxy_server_ip}}', args.proxy_server_ip))
