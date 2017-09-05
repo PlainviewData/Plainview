41e83815c75773d68d5b41f0626ddb5c708a814d
Daniel <db2791@nyu.edu>
2017-09-04 21:15:18 -0500

    dsa

--- webserver/nginx_config.py
+++ webserver/nginx_config.py
@@ -7,8 +7,8 @@ parser.add_argument('--client_server_ip')
 parser.add_argument('--proxy_server_ip')
 args = parser.parse_args()
 
-with open("default_template.txt", "rt") as fin:
-    with open("default", "wt") as fout:
+with open("/home/ubuntu/plainview/webserver/default_template.txt", "rt") as fin:
+    with open("/home/ubuntu/plainview/webserver/default", "wt") as fout:
         for line in fin:
             newline = line.replace('{{api_server_ip}}', args.api_server_ip)
             newline = newline.replace('{{client_server_ip}}', args.client_server_ip)
