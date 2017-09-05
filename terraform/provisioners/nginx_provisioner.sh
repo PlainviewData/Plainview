53e1d75c9cc54b63bbc90e0fd7c23f1cf12b0694
Daniel <db2791@nyu.edu>
2017-09-04 19:48:57 -0500

    updated provisioner

--- terraform/provisioners/nginx_provisioner.sh
+++ terraform/provisioners/nginx_provisioner.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
-while [[ $# -gt 1 ]]
-do
-key="$1"
-
-case $key in
-    -a|--api_server_ip)
-    api_server_ip="$2"
-    shift # past argument
+while getopts ":a:p:c:" opt; do
+  case $opt in
+    a) api_server_ip="$OPTARG"
     ;;
-    -c|--client_server_ip)
-    client_server_ip="$2"
-    shift # past argument
+    p) proxy_server_ip="$OPTARG"
     ;;
-    -p|--proxy_server_ip)
-    proxy_server_ip="$2"
-    shift # past argument
+    c) client_server_ip="$OPTARG"
     ;;
-    --default)
-    DEFAULT=YES
-    ;;
-    *)
-            # unknown option
-    ;;
-esac
-shift # past argument or value
+  esac
 done
 
 sudo apt-get update
--- webserver/nginx_config.py
+++ webserver/nginx_config.py
@@ -7,17 +7,10 @@ parser.add_argument('--client_server_ip')
 parser.add_argument('--proxy_server_ip')
 args = parser.parse_args()
 
-with open(os.path.join("/home/ubuntu/plainview/webserver", "default_template.txt"), "rt") as fin:
-    with open(os.path.join("/home/ubuntu/plainview/webserver", "default"), "wt") as fout:
+with open("default_template.txt", "rt") as fin:
+    with open("default", "wt") as fout:
         for line in fin:
-            fout.write(line.replace('{{api_server_ip}}', args.api_server_ip))
-
-with open(os.path.join("/home/ubuntu/plainview/webserver", "default"), "rt") as fin:
-    with open(os.path.join("/home/ubuntu/plainview/webserver", "default"), "wt") as fout:
-        for line in fin:
-            fout.write(line.replace('{{client_server_ip}}', args.client_server_ip))
-
-with open(os.path.join("/home/ubuntu/plainview/webserver", "default"), "rt") as fin:
-    with open(os.path.join("/home/ubuntu/plainview/webserver", "default"), "wt") as fout:
-        for line in fin:
-            fout.write(line.replace('{{proxy_server_ip}}', args.proxy_server_ip))
+            newline = line.replace('{{api_server_ip}}', args.api_server_ip)
+            newline = newline.replace('{{client_server_ip}}', args.client_server_ip)
+            newline = newline.replace('{{proxy_server_ip}}', args.proxy_server_ip)
+            fout.write(newline)
