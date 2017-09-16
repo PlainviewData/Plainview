0eb05341fb6e48639662a996d4089cf2ca370af6
Daniel <db2791@nyu.edu>
2017-09-16 17:47:43 -0500

    client provision

--- client/client_config.py
+++ client/client_config.py
@@ -1,14 +1,15 @@
 import os
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--api_server_ip')
 args = parser.parse_args()
 
-with open("config/api_server.json", "r") as jsonFile:
+with open("/home/bitnami/plainview/client/config/api_server.json", "r") as jsonFile:
     data = json.load(jsonFile)
 
-data["ip"] = args.api_server_ip
+data["ip"] = args.api_server_ip+":4000"
 
-with open("config/api_server.json", "w") as jsonFile:
+with open("/home/bitnami/plainview/client/config/api_server.json", "w") as jsonFile:
     json.dump(data, jsonFile)
