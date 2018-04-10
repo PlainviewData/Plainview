1368f7ba01984027085969b5bc09ea3588c3e4a9
PlainviewData <dbalagula@nyu.edu>
2018-04-10 18:04:28 -0500

    Update api.js

--- client/public/pvstatic/js/api.js
+++ client/public/pvstatic/js/api.js
@@ -14,7 +14,15 @@ $("#urlInputForm").submit(function(event) {
 			// document.write(res);
 			// document.close();
 
-			window.location = ("/"+res.body.news_site_url); 
+			$("#article_headline").html(res.body.original_headline);
+			$("#article_url").html(res.body.news_site_url);
+			$("#article_date_posted").html(res.body.original_date_posted);
+			$("#article_authors").html(res.body.original_authors);
+			$("#article_story").html(res.body.original_story_content);
+
+			$('#redirect_to_archive').click(function(){
+				window.location = ("/"+res.body.news_site_url); 
+			 })
 
 		}
 	});
